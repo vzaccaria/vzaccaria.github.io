1f00e7aa98aee1e30574a96648ea57362ad1f22c
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-03-17 12:23:52 +0100

    update: remove superagent and bluebird

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+/* eslint-env browser */
+
+import Promise from 'promise-polyfill'; 
+
+// To add to window
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
+import 'whatwg-fetch';
+
+
 import React from "react";
 import { render } from "react-dom";
 
