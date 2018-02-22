c630334f77d5f730d26cdec1f519950c2987bae1
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-02-22 15:06:20 +0100

    minor: changes (client.js, client.js.gz, bibliov2.json, sidebar.jsx)

--- src/js/pages/components/sidebar.jsx
+++ src/js/pages/components/sidebar.jsx
@@ -43,7 +43,7 @@ export default class Sidebar extends statefulComponent {
             <SidebarLinkItem
               icon="fa-user"
               name="CV"
-              link="https://www.dropbox.com/s/gh4x3viv3qvqzpg/cv.pdf?dl=0"
+              link="https://www.dropbox.com/s/z6xi289k89w36ru/cv.pdf?dl=0"
             />
             <SidebarLinkItem
               icon="fa-youtube-play"
