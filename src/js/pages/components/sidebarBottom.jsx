de20d4b92d94ece9ffaf529c28d2dfc3591b44d8
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-07-26 10:17:40 +0200

    New core asset deploy

--- src/js/pages/components/sidebarBottom.jsx
+++ src/js/pages/components/sidebarBottom.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React from "react";
 
 class SidebarBottomButton extends React.Component {
-    render() {
-        return (
-            <div className="sidebar__bottom__button" onClick={() => location.href = this.props.link} style={{cursor: "pointer"}} >
-                <i className={`fa ${this.props.icon}`} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className="sidebar__bottom__button"
+        onClick={() => (location.href = this.props.link)}
+        style={{ cursor: "pointer" }}
+      >
+        <i className={`fa ${this.props.icon}`} />
+      </div>
+    );
+  }
 }
 
 class SidebarBottomBuiltWith extends React.Component {
-    render() {
-        return (<a className="sidebar__bottom__builtwith__container" href="https://facebook.github.io/react/">
-                <div className="sidebar__bottom__builtwith__text" >Built with React</div><br/>
-                <div className="sidebar__bottom__builtwith__text" >on {__BUILDDATE__}</div>
+  render() {
+    return (
+      <div>
+        <a
+          className="sidebar__bottom__builtwith__container"
+          href="https://facebook.github.io/react/"
+        >
+          <div className="sidebar__bottom__builtwith__text">
+            Built with React
+          </div>
         </a>
-        );
-    }
+        <div className="sidebar__bottom__builtwith__text__date">
+          on {__BUILDDATE__}
+        </div>
+      </div>
+    );
+  }
 }
 
 class SidebarBottom extends React.Component {
-    render() {
-        return (
-            <div className="sidebar__bottom">
-                {this.props.children}
-            </div>
-        )
-    }
+  render() {
+    return <div className="sidebar__bottom">{this.props.children}</div>;
+  }
 }
 
-export default { SidebarBottomButton, SidebarBottom, SidebarBottomBuiltWith }
+export default { SidebarBottomButton, SidebarBottom, SidebarBottomBuiltWith };
