f693f04b33d75d75e5062f29de819547be816ae4
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2021-06-09 09:41:55 +0200

    update: add redirect to AOS (routes.jsx)

--- src/js/routes.jsx
+++ src/js/routes.jsx
@@ -15,6 +15,10 @@ let history = createHashHistory({
   queryKey: false
 });
 
+let goAOS = () => { 
+        console.log("Forcing redirect");
+        window.location.href = 'https://www.notion.so/Advanced-Operating-Systems-A-A-2021-2022-Polimi-f77b9fb8c9ea43ea953f40c35b84593a'; return null; }
+
 class Routes extends React.Component {
   render() {
     return (
@@ -23,6 +27,7 @@ class Routes extends React.Component {
           <div>
             <Route exact path="/" component={bioPage} />
             <Route path="/research" component={researchPage} />
+            <Route path="/r/aos" component={ goAOS } />
             <Route path="/teaching" component={SlidesPage} />
             <Route path="/videos" component={videosPage} />
             <Route path="/address" component={AddressPage} />
